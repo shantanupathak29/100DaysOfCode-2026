e89330849904c02de229fecb2ca5e43a93c92932
pulkit-0715 <singhpulkit1506@gmail.com>
2026-06-22 09:01:39 +0530

    Update Day4 solutions

--- DSA/Pulkit_Singh_590022181/Day4/Question2.c
+++ DSA/Pulkit_Singh_590022181/Day4/Question2.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
+    int n;
+    scanf("%d", &n);
 
-    int arr[n][m];
+    int arr[n];
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            scanf("%d", &arr[i][j]);
-        }
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            printf("%d ", arr[j][i]);
-        }
-        printf("\n");
+    int k;
+    scanf("%d", &k);
+
+    k = k % n;
+
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
 
     return 0;
